4e4fb972ba65398b8e4bc854bfac7b62fc35c453
Spong3ibob <leonardomaran17@gmail.com>
2022-12-14 15:25:50 +0100

    prima parte completata

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => "10.4 km"
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => "2 km"
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => "1 km"
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => "5.5 km"
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => "50 m"
+        ],
+
+    ];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Hotel</title>
+</head>
+<body>
+    <h1>Hotel</h1>
+    <?php for($i = 0; $i <= 4; $i++){ ?>
+    <div class="card">
+       <h2><?php echo $hotels[$i]["name"]; ?></h2>
+       <span><?php echo $hotels[$i]["vote"]; ?><i class="fa-solid fa-star"></i></span>
+       <p><?php echo $hotels[$i]["description"]; ?></p>
+       <span><?php echo $hotels[$i]["parking"]; ?></span>
+       <span><?php echo $hotels[$i]["distance_to_center"]; ?></span>
+    </div>
+    <?php }; ?>
+</body>
+</html>
